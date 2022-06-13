71686dd9bd80c7a893769d0b03a4fa9e5406660a
nayemshaon34 <nayemshaon34@gmail.com>
2022-06-14 00:19:51 +0600

    conversion

---
+++ type_coversion.py
@@ -0,0 +1,6 @@
+birthyear = input("please enter your Birth Year ") #when we use input function, we will always get a string
+age = 2022 - int(birthyear) #conversing to int from string
+print(age)
+
+print(type(birthyear))
+print(type(age)) #to see the type of variables
