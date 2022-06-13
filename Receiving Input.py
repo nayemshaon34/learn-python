a319a490a5adfd7f53c02705698cd95c26c99c1c
nayemshaon34 <nayemshaon34@gmail.com>
2022-06-14 00:02:05 +0600

    exercise input

--- Receiving Input.py
+++ Receiving Input.py
@@ -3,3 +3,10 @@ print("Hello "+name) #concataing input with hello thats why we used + sign
 
 
 print("i am "+input("How you doing? "))
+
+
+
+#exercise 1
+name = input("what is your name? ")
+print("My name is "+name)
+print(name + " likes " +input("What is your favourite color? "))
