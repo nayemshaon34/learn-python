12336b538b9a07adc8a7a67d67e3ce17fb5793b0
nayemshaon34 <nayemshaon34@gmail.com>
2022-06-14 21:55:06 +0600

    done

--- guessing_game.py
+++ guessing_game.py
@@ -1,12 +1,25 @@
-i = 1
-Guess = int(input("Guess: "))
+# i = 1
+# Guess = int(input("Guess: "))
+#
+# while i<= 3:
+#     if Guess == 9:
+#         print("You Win!")
+#         break
+#     elif  i<3:
+#         Guess = int(input("Guess: "))
+#     else:
+#         print("Sorry You failed!")
+#     i+=1
 
-while i<= 3:
-    if Guess == 9:
-        print("You Win!")
+guess_count = 0
+secret_number = 9
+guess_limit = 3
+
+while guess_count < guess_limit:
+    guess = int(input("Guess: "))
+    guess_count += 1
+    if guess == secret_number:
+        print('You Win!')
         break
-    elif  i<3:
-        Guess = int(input("Guess: "))
-    else:
-        print("Sorry You failed!")
-    i+=1
+else: #while loop can also have else statement, if while loop completes then will go to the else loop , otherwise if the while loop is interrupted by break , then else loop will not execute
+    print("Sorry You failed!")
