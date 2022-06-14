ca1a296f26dfcfc51a133beaf53b05ac637c6204
nayemshaon34 <nayemshaon34@gmail.com>
2022-06-14 19:56:44 +0600

    logical done

--- logical_operator.py
+++ logical_operator.py
@@ -1,7 +1,13 @@
 high_income = False
 good_credit = True
+record = False
 
 if high_income and good_credit:
     print("Eligible for loan")
 else:
     print("Not eligible for loan")
+
+if good_credit and not record:
+    print('yes')
+else:
+    print('no')
