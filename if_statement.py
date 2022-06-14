592efd2358d3d12086e524498253f349d97d3c21
nayemshaon34 <nayemshaon34@gmail.com>
2022-06-14 19:17:05 +0600

    if statement done

---
+++ if_statement.py
@@ -0,0 +1,14 @@
+is_hot = True
+is_cold = True
+
+if is_hot:
+    print("It's a hot day")
+    print("drink plenty of water")
+#shift+tab
+
+elif is_cold:
+    print("It's a cold day")
+    print("Wear warm clothes")
+else:
+    print("It's a lovely day")
+print("Enjoy your day")
