fe0f07ba5ec5490b17f9d382e808dd05aab0fbf2
nayemshaon34 <nayemshaon34@gmail.com>
2022-06-14 16:58:53 +0600

    formatted string

---
+++ formatted_strings.py
@@ -0,0 +1,4 @@
+first = "John"
+last = "Cena"
+mssg = f'{first} [{last}] is pro wrestler' # prefix our strings with a f and then use curly brackets to dynamically insert values into our string
+print(mssg)
