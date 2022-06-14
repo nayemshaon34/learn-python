c40844dc0aae53a25ca86497d5993d665618fd89
nayemshaon34 <nayemshaon34@gmail.com>
2022-06-14 22:18:09 +0600

    car game done

---
+++ car_game.py
@@ -0,0 +1,21 @@
+command = ''
+#while command != 'quit':
+while True:
+    command = input("> ");
+    if command.lower() == 'help':
+        print('start - to start the car')
+        print('stop - to stop the car')
+        print('quit - to exit')
+
+    elif command.lower() == 'start':
+        print("Car started...Ready to go!")
+    elif command.lower() == 'stop':
+        print("Car stopped")
+    elif command.lower() == 'quit':
+        break;
+    else:
+        print("I don't understand that...")
+
+
+
+
