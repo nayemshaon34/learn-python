050df2ab7f798ff21f7f1f899e60aca4dacedcdb
nayemshaon34 <nayemshaon34@gmail.com>
2022-06-14 18:36:04 +0600

    math function

---
+++ math_functions.py
@@ -0,0 +1,13 @@
+import math
+
+x = 34.74
+print(round(x))
+
+y = -30.74
+print(abs(y)) #output will  be absolute value
+
+z = 30.74
+print(math.ceil(z))
+print(math.floor(z))
+
+# for more math module https://docs.python.org/3/library/math.html
