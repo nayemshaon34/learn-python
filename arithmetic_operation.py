25653a5ef0de385815e657aac78680e60437ea38
nayemshaon34 <nayemshaon34@gmail.com>
2022-06-14 17:49:08 +0600

    arithmetic

---
+++ arithmetic_operation.py
@@ -0,0 +1,6 @@
+print(10/3) #output will be floating point number
+print(10//3) #output will be integer
+print(8**3) #will work as a power function like 8^3
+x = 5
+x += 3 #augmented assignment opetaor
+print(x)
